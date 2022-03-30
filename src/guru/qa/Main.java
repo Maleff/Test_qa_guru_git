0e2d5d4a9fe62bd1b61612fb4b3798237259dbe9
Maleff <101434297+Maleff@users.noreply.github.com>
2022-03-30 23:53:44 +0300

    Update Main.java

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -3,7 +3,13 @@ package guru.qa;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, Hell-boy!");
+	System.out.println("Hello, Hell-boy!");
         System.out.println("Somthng");
     }
+	
+	
+public static void main(String[] args) {
+	System.out.println("Hello, Hell-boy!");
+	}
+	
 }
